0aaef336002af4ff28fef8c7edaa79fd985b2f6a
Couda <92357239+Coudaa@users.noreply.github.com>
2023-06-08 10:55:17 +0700

    Initial commit

---
+++ app/src/main/java/com/alfonsusjericho202102286/recycleviewb/AddMahasiswaActivity.java
@@ -0,0 +1,14 @@
+package com.alfonsusjericho202102286.recycleviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa);
+    }
+}
