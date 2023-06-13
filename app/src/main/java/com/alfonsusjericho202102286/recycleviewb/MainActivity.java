06622fecf7bddb706c6dc6ee76cad47735f782b3
Couda <92357239+Coudaa@users.noreply.github.com>
2023-06-13 10:47:31 +0700

    Tambah

--- app/src/main/java/com/alfonsusjericho202102286/recycleviewb/MainActivity.java
+++ app/src/main/java/com/alfonsusjericho202102286/recycleviewb/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.AsyncHttpClient;
@@ -17,6 +20,7 @@ import java.util.List;
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton _addButton;
     private RecyclerView _recycleview;
 
     @Override
@@ -26,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
         _recycleview = findViewById(R.id.recycleview1);
 
+        initAddButton();
+        loadRecyclerView();
+    }
+    private void loadRecyclerView(){
         AsyncHttpClient ahc = new AsyncHttpClient();
         String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
 
@@ -45,6 +53,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void initAddButton(){
+        _addButton = findViewById(R.id.addButton);
+        _addButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddMahasiswaActivity.class);
+                startActivity(intent);
+
+                loadRecyclerView();
             }
         });
     }
